74a88651adf00e48cb3cadfccfa090ec0fd15f65
Henry <h.mareck@gmail.com>
2016-06-29 02:38:00 -0500

    cleaned out but no functional changes

--- imports/components/display/showPrice.js
+++ imports/components/display/showPrice.js
@@ -8,21 +8,15 @@ import template from './showPrice.html';
 class DisplayCtrl {
 	constructor($scope) {
 		$scope.viewModel(this);
-		
-    this.helpers({
-		tasks() {
-			return Tasks.find({});
-		}
-    });
 
-    this.helpers({
-		order() {
-			return Order.find({});
-		}
-    });
 	this.baseColor = "not chosen";
 	this.test = [50,10];
 	this.tst = "other stuff";
+	
+	this.qtyButton = function(increment) {
+		var inc = increment;
+		this.quantity += inc;
+	}
 	
 	this.downOne = function() {
 		this.test[0] -= 1;
@@ -31,39 +25,10 @@ class DisplayCtrl {
 	this.calculate = function(inpa) {
 		var inpa = inpa;
 		return this.test[0]*this.newCost*inpa;
-	};
-		
-  }
-    addTask(newTask) {
-    // Insert a task into the collection
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date
-    });
- 
-    // Clear form
-    this.newTask = '';
-  }
-	setChecked(task){
-		Tasks.update(task._id, {
-			$set: {
-				checked : !task.checked
-			},
-		});
-	}
+	};	
 	
-	removeTask(task) {Tasks.remove(task._id);}
-	
-    updateQty(newQty) {
-    // Update quantity saved in the collection
-    Order.update({
-      quantity: newQty
-    });
- 
-  }  
-
+  }
 }
-
 
 export default angular.module('displayModule', [
 	angularMeteor
