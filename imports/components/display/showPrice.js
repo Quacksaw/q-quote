ebe18eca6a4261f665a8f5c4b1d63e6d227564ed
Henry <h.mareck@gmail.com>
2016-07-16 18:09:03 -0500

    Starting on Collections . . slowly

--- imports/components/display/showPrice.js
+++ imports/components/display/showPrice.js
@@ -1,17 +1,18 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import { Order } from '../../api/order.js';
-import { Tasks } from '../../api/tasks.js';
 
 import template from './showPrice.html';
 
 class DisplayCtrl {
 	constructor($scope) {
 		$scope.viewModel(this);
+		
 	this.quantity = 12;
 	this.baseColor = "not chosen";
 	this.baseCost = [4,0];
 	this.printColors = 0;
+	this.costPerColor = 1;
 	
 	this.shortSleeve = function() {
 		this.baseCost[0] = 4;
@@ -20,6 +21,7 @@ class DisplayCtrl {
 	this.longSleeve = function() {
 		this.baseCost[0] = 7;
 		this.shirtStyle = "Long Sleeve";
+		
 	};
 	this.tankTop = function() {
 		this.baseCost[0] = 5;
@@ -32,29 +34,38 @@ class DisplayCtrl {
 	this.calculate = function() {
 		if (this.baseColor === "White"){
 			this.baseCost[1]=0;
-			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors;
+			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors*this.costPerColor;
 			var fCost = eaCost*this.quantity;
 			return fCost;
 			}
 		else if (this.baseColor === "Light"){
 			this.baseCost[1]=1;
-			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors;
+			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors*this.costPerColor;
 			var fCost = eaCost*this.quantity;
 			return fCost;
 			}
 		else if (this.baseColor === "Dark"){
 			this.baseCost[1]=2;
-			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors;
+			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors*this.costPerColor;
 			var fCost = eaCost*this.quantity;
 			return fCost;
 			}
 		else {
-			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors;
+			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors*this.costPerColor;
 			var fCost = eaCost*this.quantity;
 			return fCost;
 		 }
 	};	
-  }
+
+	// db.order.insert({ thingy: "Value One!", thinger: [5,0] }); //
+		this.helpers({
+			order() {
+				return Order.find({});
+			}
+		})
+
+		
+	}
 }
 
 export default angular.module('displayModule', [
