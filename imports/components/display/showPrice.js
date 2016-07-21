071d23802af8c82a0dadb3e723864b99538230c3
Henry Mareck <Quacksaw@users.noreply.github.com>
2016-07-21 16:46:15 -0500

    Add files via upload

--- imports/components/display/showPrice.js
+++ imports/components/display/showPrice.js
@@ -10,21 +10,15 @@ class DisplayCtrl {
 		
 	this.quantity = 12;
 	this.baseColor = "not chosen";
-	this.baseCost = [4,0];
 	this.printColors = 0;
-	this.costPerColor = 1;
-	this.idSearch = "";
 	
 	this.shortSleeve = function() {
-		this.baseCost[0] = 4;
 		this.shirtStyle = "Short Sleeve";
 	};
 	this.longSleeve = function() {
-		this.baseCost[0] = 7;
 		this.shirtStyle = "Long Sleeve";		
 	};
 	this.tankTop = function() {
-		this.baseCost[0] = 5;
 		this.shirtStyle = "Tank Top";
 	};
 	this.costEa = function() {
@@ -33,43 +27,34 @@ class DisplayCtrl {
 	}
 	this.calculate = function() {
 		if (this.baseColor === "White"){
-			this.baseCost[1]=0;
-			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors*this.costPerColor;
+			var eaCost = this.printColors*this.order[0].cpc;
 			var fCost = eaCost*this.quantity;
 			return fCost;
 			}
 		else if (this.baseColor === "Light"){
-			this.baseCost[1]=1;
-			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors+this.costPerColor;
+			var eaCost = this.printColors*this.order[0].cpc;
 			var fCost = eaCost*this.quantity;
 			return fCost;
 			}
 		else if (this.baseColor === "Dark"){
-			this.baseCost[1]=2;
-			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors*this.costPerColor;
+			var eaCost = this.printColors*this.order[0].cpc;
 			var fCost = eaCost*this.quantity;
 			return fCost;
 			}
 		else {
-			var eaCost = this.baseCost[0]+this.baseCost[1]+this.printColors*this.costPerColor;
+			var eaCost = this.printColors*this.order[0].cpc;
 			var fCost = eaCost*this.quantity;
 			return fCost;
 		 }
 	};	
-
-	// db.order.insert({ thingy: "Value One!", thinger: [5,0] }); //
-	// db.order.insert({ _id: "calcValues", item: "box", qty: 20 }) //
-	// db.order.insert({ _id: "calcValues2", item: "box", qty: 20 }) //
-	// db.order.insert({ _id: "calcValues3", shirtColorings: "Blue", qtyCheckpt: [20,50,100] }) //
-	
 		this.helpers({
-			order() {
+			order2() {
 				return Order.find({});
 			}
 		}),
 		this.helpers({
-			order2() {
-				return Order.find({name:this.getReactively('nameSearch')});
+			order() {
+				return Order.find({name:this.getReactively('currentProfile')});
 			}
 		})		
 	};
@@ -84,7 +69,8 @@ class DisplayCtrl {
 		);	
 		this.newSS = '';
 		this.newLS = '';	
-		this.newName = '';		
+		this.newName = '';
+		this.costPerColor = '';
 	}	
 }
 
